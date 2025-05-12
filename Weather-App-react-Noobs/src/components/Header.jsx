68c35cb3f2ea5e91926898b9e4190f8c1984acf7
Muhammad Fachry Gunawan <fahrignwn21@gmail.com>
2025-05-12 22:33:58 +0700

    real time date

--- Weather-App-react-Noobs/src/components/Header.jsx
+++ Weather-App-react-Noobs/src/components/Header.jsx
@@ -1,8 +1,12 @@
-const Header = ({ location, date }) => {
+import { getCurrentDate } from './TimeDisplay';
+
+const Header = ({ location }) => {
+  const currentDate = getCurrentDate();
+
   return (
     <div className="flex justify-between items-center mb-12">
       <h1 className="text-xl font-medium text-gray-700">{location}</h1>
-      <p className="text-xl font-bold text-gray-600 mx-auto">{date}</p>
+      <p className="text-xl font-bold text-gray-600 mx-auto">{currentDate}</p>
     </div>
   );
 };
--- Weather-App-react-Noobs/src/components/TimeDisplay.jsx
+++ Weather-App-react-Noobs/src/components/TimeDisplay.jsx
@@ -1,14 +1,23 @@
 import { useState, useEffect } from 'react';
 
+export const getCurrentDate = () => {
+  const today = new Date();
+  return today.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+};
+
 const CurrentTime = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentTime(new Date());
-    }, 1000); 
+    }, 1000);
 
-    return () => clearInterval(timer); 
+    return () => clearInterval(timer);
   }, []);
 
   const formattedTime = currentTime.toLocaleTimeString('en-US', {
